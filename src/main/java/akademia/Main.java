bf8c0ccb8a2e5690894c156d05a6b8897a6054fa
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-02-22 13:38:25 +0100

    add scanner

--- src/main/java/akademia/Main.java
+++ src/main/java/akademia/Main.java
@@ -1,8 +1,15 @@
 package akademia;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+   Scanner scanner = new Scanner(System.in);
+    System.out.println("jak masz na imię?");
+    String sc = scanner.nextLine();
+    System.out.println("Cześć " + sc);
   }
 }

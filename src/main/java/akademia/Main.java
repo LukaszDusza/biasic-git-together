2ef4c700a196592012eb327c423b476cad8e3af8
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-02-22 13:50:00 +0100

    add koniec poprawki

--- src/main/java/akademia/Main.java
+++ src/main/java/akademia/Main.java
@@ -6,12 +6,10 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
-
     Scanner scanner = new Scanner(System.in);
     System.out.println("jak masz na imię?");
     String sc = scanner.nextLine();
     System.out.println("Cześć " + sc);
-
     System.out.println("koniec");
   }
 }
